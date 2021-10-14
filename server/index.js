6f90566ed0f22501d628df984c039ec0328f3268
Jesse Sites <jesse318s@gmail.com>
2021-10-14 17:56:49 -0500

    Simplified server code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
 const articles = require("./routes/articles");
-const connection = require("./db");
 const cors = require("cors");
 const express = require("express");
 const app = express();
 
-connection();
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => { console.log("MongoDB database connection established successfully.") });
 
 app.use(express.json());
 app.use(cors());
