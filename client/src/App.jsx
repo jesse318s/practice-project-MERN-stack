09e4c4f64c3a6caf5b6254dc5f1522e91c974eeb
jesse318s <jesse318s@gmail.com>
2024-10-02 22:07:52 -0500

    update class comp to func comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import {
   addArticle,
   getArticles,
@@ -7,211 +7,204 @@ import {
   deleteArticle,
 } from "./services/articleServices";
 
-//component
-class App extends Component {
-  //variables
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [currentBody, setCurrentBody] = useState("");
 
-  //sets state
-  state = { articles: [], currentTitle: "", currentBody: "" };
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const { data } = await getArticles();
 
-  //functions
+        setArticles(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchArticles();
+  }, []);
 
-  //retrieves articles on load
-  async componentDidMount() {
-    try {
-      const { data } = await getArticles();
-      this.setState({ articles: data });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  //handles new article form input change
-  handleChangeTitle = ({ currentTarget: input }) => {
-    this.setState({ currentTitle: input.value });
+  const handleChangeTitle = (e) => {
+    setCurrentTitle(e.currentTarget.value);
   };
 
-  handleChangeBody = ({ currentTarget: input }) => {
-    this.setState({ currentBody: input.value });
+  const handleChangeBody = (e) => {
+    setCurrentBody(e.currentTarget.value);
   };
 
-  //handles add article
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const originalArticles = this.state.articles;
+
+    const originalArticles = articles;
+
     try {
       const { data } = await addArticle({
-        title: this.state.currentTitle,
-        body: this.state.currentBody,
+        title: currentTitle,
+        body: currentBody,
       });
-      const articles = originalArticles;
-      articles.push(data);
-      this.setState({ articles, currentTitle: "", currentBody: "" });
+
+      setArticles([...originalArticles, data]);
+      setCurrentTitle("");
+      setCurrentBody("");
     } catch (error) {
       console.log(error);
     }
   };
 
-  //handles check/uncheck article
-  handleUpdate = async (currentTitle) => {
-    const originalArticles = this.state.articles;
+  const handleUpdate = async (currentTitle) => {
+    const originalArticles = articles;
+
     try {
-      const articles = [...originalArticles];
-      const index = articles.findIndex(
+      const updatedArticles = [...originalArticles];
+      const index = updatedArticles.findIndex(
         (article) => article._id === currentTitle
       );
-      articles[index] = { ...articles[index] };
-      articles[index].completed = !articles[index].completed;
-      this.setState({ articles });
+
+      updatedArticles[index] = { ...updatedArticles[index] };
+      updatedArticles[index].completed = !updatedArticles[index].completed;
+      setArticles(updatedArticles);
       await updateArticle(currentTitle, {
-        completed: articles[index].completed,
+        completed: updatedArticles[index].completed,
       });
     } catch (error) {
-      this.setState({ articles: originalArticles });
+      setArticles(originalArticles);
       console.log(error);
     }
   };
 
-  //handles edit article
-  handleEdit = async (currentTitle) => {
-    const originalArticles = this.state.articles;
+  const handleEdit = (currentTitle) => {
+    const originalArticles = articles;
+
     try {
-      const articles = [...originalArticles];
-      const index = articles.findIndex(
+      const updatedArticles = [...originalArticles];
+      const index = updatedArticles.findIndex(
         (article) => article._id === currentTitle
       );
-      articles[index] = { ...articles[index] };
-      this.state.currentTitle = articles[index].title;
-      this.state.currentBody = articles[index].body;
-      this.setState({ articles });
+
+      updatedArticles[index] = { ...updatedArticles[index] };
+      setCurrentTitle(updatedArticles[index].title);
+      setCurrentBody(updatedArticles[index].body);
+      setArticles(updatedArticles);
     } catch (error) {
-      this.setState({ articles: originalArticles });
+      setArticles(originalArticles);
       console.log(error);
     }
   };
 
-  //handles delete article
-  handleDelete = async (currentTitle) => {
-    const originalArticles = this.state.articles;
+  const handleDelete = async (currentTitle) => {
+    const originalArticles = articles;
+
     try {
-      const articles = originalArticles.filter(
+      const updatedArticles = originalArticles.filter(
         (article) => article._id !== currentTitle
       );
-      this.setState({ articles });
+
+      setArticles(updatedArticles);
       await deleteArticle(currentTitle);
     } catch (error) {
-      this.setState({ articles: originalArticles });
+      setArticles(originalArticles);
       console.log(error);
     }
   };
 
-  render() {
-    //constants
-
-    //declares articles
-    const { articles } = this.state;
-
-    // render component
-    return (
-      <>
-        {/* articles app frontend */}
-        <div className="flex">
-          <div className="app_container">
-            <h1 className="heading">Create Post</h1>
-            {/* new article form */}
-            <form
-              onSubmit={this.handleSubmit}
-              style={{ textAlign: "center", margin: "10px" }}
-            >
-              <input
-                type="text"
-                style={{ width: "100%" }}
-                value={this.state.currentTitle}
-                required={true}
-                onChange={this.handleChangeTitle}
-                placeholder="Create New Title"
-              />
-              <textarea
-                rows="20"
-                style={{ width: "100%" }}
-                value={this.state.currentBody}
-                onChange={this.handleChangeBody}
-                placeholder="Create New Body"
-              />
-              <br />
-              <input
-                style={{ height: "40px", color: "red" }}
-                type="reset"
-                value="Clear"
-                onClick={() => {
-                  if (
-                    window.confirm(
-                      "Are you sure you wish to clear this post? This cannot be undone."
-                    )
+  return (
+    <>
+      <div className="flex">
+        <div className="app_container">
+          <h1 className="heading">Create Post</h1>
+          <form
+            onSubmit={handleSubmit}
+            style={{ textAlign: "center", margin: "10px" }}
+          >
+            <input
+              type="text"
+              style={{ width: "100%" }}
+              value={currentTitle}
+              required={true}
+              onChange={handleChangeTitle}
+              placeholder="Create New Title"
+            />
+            <textarea
+              rows="20"
+              style={{ width: "100%" }}
+              value={currentBody}
+              onChange={handleChangeBody}
+              placeholder="Create New Body"
+            />
+            <br />
+            <input
+              style={{ height: "40px", color: "red" }}
+              type="reset"
+              value="Clear"
+              onClick={() => {
+                if (
+                  window.confirm(
+                    "Are you sure you wish to clear this post? This cannot be undone."
                   )
-                    this.setState({ currentTitle: "", currentBody: "" });
-                }}
-              />
-              <input
-                style={{ height: "40px" }}
-                type="submit"
-                value="Submit Post"
-              />
-            </form>
-            {/* displays stored articles */}
-            <h1 className="heading">Posts</h1>
-            <div>
-              {articles.map((article) => (
-                <div key={article._id} className="flex article_container">
-                  <input
-                    type="checkbox"
-                    checked={article.completed}
-                    onChange={() => this.handleUpdate(article._id)}
-                  />
-                  <div className="article">
-                    <div
-                      style={{
-                        paddingTop: "10px",
-                        position: "relative",
-                        top: "15px",
-                      }}
-                      className={article.completed ? "article_completed" : ""}
-                    >
-                      {article.title}
-                    </div>
-                    <br />
-                    Submitted: {article.createdAt.slice(0, 10)}
-                    <br />
-                    <div style={{ paddingTop: "10px" }}>{article.body}</div>
-                    <br />
-                    <button
-                      style={{ color: "blue" }}
-                      onClick={() => this.handleEdit(article._id)}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      style={{ color: "red" }}
-                      onClick={() => {
-                        if (
-                          window.confirm(
-                            "Are you sure you wish to delete this item? This cannot be undone."
-                          )
-                        )
-                          this.handleDelete(article._id);
-                      }}
-                    >
-                      Delete
-                    </button>
+                )
+                  setCurrentTitle("");
+                setCurrentBody("");
+              }}
+            />
+            <input
+              style={{ height: "40px" }}
+              type="submit"
+              value="Submit Post"
+            />
+          </form>
+          <h1 className="heading">Posts</h1>
+          <div>
+            {articles.map((article) => (
+              <div key={article._id} className="flex article_container">
+                <input
+                  type="checkbox"
+                  checked={article.completed}
+                  onChange={() => handleUpdate(article._id)}
+                />
+                <div className="article">
+                  <div
+                    style={{
+                      paddingTop: "10px",
+                      position: "relative",
+                      top: "15px",
+                    }}
+                    className={article.completed ? "article_completed" : ""}
+                  >
+                    {article.title}
                   </div>
+                  <br />
+                  Submitted: {article.createdAt.slice(0, 10)}
+                  <br />
+                  <div style={{ paddingTop: "10px" }}>{article.body}</div>
+                  <br />
+                  <button
+                    style={{ color: "blue" }}
+                    onClick={() => handleEdit(article._id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    style={{ color: "red" }}
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "Are you sure you wish to delete this item? This cannot be undone."
+                        )
+                      )
+                        handleDelete(article._id);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default App;
